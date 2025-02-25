2940d0856662b3d6c88206c01f63df578b27ea57
suShinGG <chin.lnw@gmail.com>
2025-02-25 17:14:30 +0700

    Update keymap.c

--- keyboards/cheapino/keymaps/sushingg/keymap.c
+++ keyboards/cheapino/keymaps/sushingg/keymap.c
@@ -38,17 +38,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MOU] = LAYOUT_split_3x5_3(
     //  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   /***/   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
-          QK_BOOT, KC_NO,   VSCODE_FORMAT,   KC_NO,   KC_NO,     /***/   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
-          KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,     /***/   OBJECT_OPERATOR,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, 
-          KC_NO,   KC_RALT, KC_NO,   KC_NO,   KC_NO,     /***/   DB_ARROW_OPERATOR,   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, 
+          QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,     /***/   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
+          KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,     /***/   KC_NO,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, 
+          KC_NO,   KC_RALT, KC_NO,   KC_NO,   KC_NO,     /***/   KC_NO,   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, 
                                    KC_NO,   KC_NO,   KC_NO,     /***/   KC_BTN2, KC_BTN1, KC_BTN3
     ),
 
     [_MED] = LAYOUT_split_3x5_3(
     //  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   /***/   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
-          QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,     /***/   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, 
-          KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,     /***/   KC_NO,   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, 
-          KC_NO,   KC_RALT, KC_NO,   KC_NO,   KC_NO,     /***/   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
+          QK_BOOT, KC_NO,   VSCODE_FORMAT,   KC_NO,   KC_NO,     /***/   RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, 
+          KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,     /***/   OBJECT_OPERATOR,   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, 
+          KC_NO,   KC_RALT, KC_NO,   KC_NO,   KC_NO,     /***/   DB_ARROW_OPERATOR,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   
                                    KC_NO,   KC_NO,   KC_NO,     /***/   KC_MSTP, KC_MPLY, KC_MUTE
     ),
 
@@ -125,7 +125,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case OBJECT_OPERATOR:
             if (record->event.pressed) {
-                SEND_STRING("->!");
+                SEND_STRING("->");
             }
             break;
     
